8f57fd78209830f6e7bb8576ba67355414291ef8
cuishunbiao <860026526@qq.com>
2023-10-07 09:49:21 +0800

    fix: keywords

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,7 +10,7 @@ export default defineNuxtConfig({
       title: 'Emoji Kitchen Online',
       meta: [
         { name: 'description', content: 'Pick two emojis you really like, then blend them together to create a brand new emoji. Create fun custom emojis!' },
-        { name: 'keyworld', content: 'emoji kitchen, emoji kitchen online' }
+        { name: 'keywords', content: 'emoji kitchen iphone, emoji kitchen apple, emojie kitchen, emoji kitchen descargar' }
       ],
     }
   }
