b6c442f6678f3adbb81b1e19c7cef507b2e3fa19
cuishunbiao <860026526@qq.com>
2024-05-20 22:36:04 +0800

    feat: add monetag

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,11 +12,6 @@ export default defineNuxtConfig({
       meta: [
         { name: 'description', content: 'Pick two emojis you really like, then blend them together to create a brand new emoji. Create fun custom emojis!' },
         { name: 'keywords', content: 'emoji kitchen browser, emoji kitchen online, emoji kitchen apple, emojie kitchen, emoji kitchen list' }
-      ],
-      script: [
-        { async: true, src: 'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1504730712788512', crossorigin: 'anonymous' },
-        { async: true, src: 'https://www.googletagmanager.com/gtag/js?id=G-VY2KECY47Y' },
-        { type: 'text/javascript', innerHTML: "window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', 'G-VY2KECY47Y');" }
       ]
     },
   }
